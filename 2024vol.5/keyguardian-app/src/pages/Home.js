4f74c2b3449d6b46a2de1641033e9579b96c2aa3
Kento <kentokentokento913@gmail.com>
2024-05-19 15:01:48 +0900

    Update Home.js

--- 2024vol.5/keyguardian-app/src/pages/Home.js
+++ 2024vol.5/keyguardian-app/src/pages/Home.js
@@ -7,7 +7,9 @@ import { Opening } from './opening.js'
 import { Closed } from './closed.js'
 
 const Home = () => {
-
+    const [isOpened, setIsOpened] = useState(false);
+    const url = useLocation().pathname;
+    const timestamp = new Date("2024-05-17T12:34:56Z");
 
     useEffect(() => {
         async function fetchData() {
@@ -23,10 +25,6 @@ const Home = () => {
         fetchData();
     }, []);
 
-    const [isOpened, setIsOpened] = useState(false);
-    const url = useLocation().pathname;
-    const timestamp = new Date("2024-05-17T12:34:56Z");
-    
     function TimeEdit(timestamp){
         const date = timestamp
         const year = date.getFullYear();
@@ -37,7 +35,7 @@ const Home = () => {
         return `${year}/${month}/${day} ${hours}:${minutes}`;
     }
 
-    const time = TimeEdit(timestamp)
+    const time = TimeEdit(timestamp);
 
     return (
         <div>
@@ -46,14 +44,14 @@ const Home = () => {
                 <h1 className="header-title">KeyGuardian</h1>
             </header>
             <main className="Home-main">
-                    <h2 className="main-message">
-                        ようこそKeyGuardianへ<br/>
-                        現在の○○研究室の開閉状態は
-                    </h2>
-                    {isOpened ? <Opening time={time}/> : <Closed time={time}/>}
-                </main>
+                <h2 className="main-message">
+                    ようこそKeyGuardianへ<br/>
+                    現在の○○研究室の開閉状態は
+                </h2>
+                {isOpened ? <Opening time={time}/> : <Closed time={time}/>}
+            </main>
         </div>
-    )
+    );
 }
 
 export default Home;
